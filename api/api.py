b6a922b308a7543fb2ed11fbdd62bab22b1c044f
Matheus-Du <duncanmatheus905@gmail.com>
2024-07-22 15:17:37 -0400

    Added routing for blog and mainpage elements.

--- api/api.py
+++ api/api.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template("home.html")
+
+@app.route("/blog")
+def blogHome():
+    return render_template("/blog/blogHome.html")
