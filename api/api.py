5032f5a50e792aa07f1bde42d574618bf26dc358
Matheus-Du <duncanmatheus905@gmail.com>
2024-07-24 13:54:10 -0400

    Gunicorn should be working.

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 app = Flask(__name__)
+
+#app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.route("/")
 def main():
@@ -10,3 +13,6 @@ def main():
 @app.route("/blog")
 def blogHome():
     return render_template("/blog/blogHome.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
