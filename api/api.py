bb69e2a18e8ce83b6792cd9ccf1f10db606cdc5d
Matheus-Du <duncanmatheus905@gmail.com>
2024-07-24 21:01:17 -0400

    Added routing and 404 page.

--- api/api.py
+++ api/api.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template, request
-from werkzeug.middleware.proxy_fix import ProxyFix
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-#app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.route("/")
 def main():
@@ -13,6 +9,10 @@ def main():
 @app.route("/blog")
 def blogHome():
     return render_template("/blog/blogHome.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("/pages/404.html"), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
