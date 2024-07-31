45e722117dc11a46ad7669932ebd5edc5ca081e8
Matheus-Du <duncanmatheus905@gmail.com>
2024-07-31 14:46:29 -0400

    Homepage gets real list of recent likes.

--- api/api.py
+++ api/api.py
@@ -10,9 +10,16 @@ htmx.init_app(app)
 def get_hn_posts():
     conn = MongoClient(f"mongodb://{os.environ['MONGOUN']}:{os.environ['MONGOPW']}@mongo:27017/")
     db = conn.personal_site
-    hn_posts = db.upvoted_posts.find().limit(5)
-    post_data = [post for post in hn_posts]
-    return f"{post_data}"
+    hn_posts = [post for post in db.upvoted_posts.find().limit(5)]
+    posts_html = []
+    for post in hn_posts:
+        #posts_html.append(f"Title: {post.get('title')}\nLink: {post.get('link')}\nDate: {post.get('link')}\nDisc: {post.get('discussion_link')}")
+        posts_html.append(render_template("elements/recentLike.html",
+                                          title=post.get('title'), 
+                                          link=post.get('link'), 
+                                          date=post.get('date_posted'), 
+                                          hn_link=post.get('discussion_link')))
+    return "".join(posts_html)
 
 @app.route("/")
 def main():
@@ -20,8 +27,11 @@ def main():
 
 @app.route("/upvotedPosts")
 def upvoted():
-    posts = get_hn_posts()
-    return posts
+    if htmx:
+        posts = get_hn_posts()
+        return posts
+    else:
+        return "Error getting recently liked posts"
 
 @app.route("/blog")
 def blogHome():
@@ -30,7 +40,7 @@ def blogHome():
 @app.route("/getRecentLikes")
 def showLikesInfo():
     if htmx:
-        return render_template("/elements/recentLike.html")
+        return render_template("/elements/recentLikes.html")
     return Response(status=400)
 
 @app.errorhandler(404)
